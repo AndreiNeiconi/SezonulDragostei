b7e22172e1a6f133d77ae7ef73246a6f16e84d8d
Neiconi Andrei <a.neiconi19@gmail.com>
2025-08-27 15:45:35 +0300

    subscribe service

--- src/app/Services/subscribe.service.ts
+++ src/app/Services/subscribe.service.ts
@@ -8,7 +8,6 @@ export class SubscribeService {
   private baseUrl = 'http://localhost:3000/api';
 
   constructor(private http: HttpClient) {}
-
   SubscribeForm(formData: FormData) {
     return this.http.post(`${this.baseUrl}/subscribe`, formData);
   }
