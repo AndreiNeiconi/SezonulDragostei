571e71f7d05f89317e26bdbbb84439f9d42cf2c2
Neiconi Andrei <a.neiconi19@gmail.com>
2025-09-01 14:57:27 +0300

    create the db

--- src/app/Services/auth.service.ts
+++ src/app/Services/auth.service.ts
@@ -1,32 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
-@Injectable({ providedIn: 'root' })
+@Injectable({
+  providedIn: 'root',
+})
 export class AuthService {
-  private baseUrl = 'http://localhost:3000/api';
-  private isLogin = false;
-
   constructor(private http: HttpClient) {}
 
-  login(email: string, password: string) {
-    this.isLogin = true
-    return this.http.post<{ token: string }>(`${this.baseUrl}/login`, { email, password });
-  }
-
-  saveToken(token: string) {
-    localStorage.setItem('token', token);
-  }
-
-  getToken() {
-    return localStorage.getItem('token');
-  }
-
-  isLoggedIn():boolean {
-    return this.isLogin
-  }
-
-  logout() {
-    this.isLogin = false
-    localStorage.removeItem('token');
+  getTime(): Observable<any> {
+    return this.http.get('/.netlify/functions/db');
   }
 }
