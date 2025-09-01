758773b0a82084296cc0165d14970f4d3e2aa097
Neiconi Andrei <a.neiconi19@gmail.com>
2025-09-01 16:01:12 +0300

    clean up and fix bugs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,16 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { SubscribePageComponent } from './subscribe-page/subscribe-page.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { AboutPageComponent } from './about-page/about-page.component';
-import { LogInPageComponent } from './log-in-page/log-in-page.component';
+
 import { DashbordComponent } from './dashbord/dashbord.component';
-import { AuthGurdService } from './Services/auth-gurd.service';
+
 
 
 const routes: Routes = [
   {path:"",component:HomePageComponent},
   { path: "subscribe", component: SubscribePageComponent },
   {path:"about",component:AboutPageComponent},
-  {path:"login",component:LogInPageComponent},
   //  {path:"dashbord",component:DashbordComponent,canActivate:[AuthGurdService]},
   {path:'dashbord',component:DashbordComponent}
 ];
